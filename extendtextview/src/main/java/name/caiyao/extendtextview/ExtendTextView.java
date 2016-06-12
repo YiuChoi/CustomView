8e48ac42763e60eda462861a66f4fa838cdd7cc6
YiuChoi <whucaiyao@gmail.com>
2016-06-12 17:30:15 +0800

    Initial commit

---
+++ extendtextview/src/main/java/name/caiyao/extendtextview/ExtendTextView.java
@@ -0,0 +1,75 @@
+package name.caiyao.extendtextview;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * 自定义View
+ * <p>
+ * Created by 蔡小木 on 2016/6/12 0012.
+ */
+
+public class ExtendTextView extends TextView {
+
+    private Paint mPaint;
+    private Paint mPaint1;
+    private Paint mPaint2;
+    private int mViewWidth;
+    private LinearGradient mLinearGradient;
+    private Matrix mMatrix;
+    private float mTranslate;
+
+    public ExtendTextView(Context context) {
+        super(context);
+    }
+
+    public ExtendTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ExtendTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public ExtendTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (mViewWidth == 0) {
+            mViewWidth = getMeasuredWidth();
+            if (mViewWidth > 0) {
+                mPaint = getPaint();
+                mLinearGradient = new LinearGradient(0, 0, mViewWidth, 0, new int[]{Color.BLUE, 0xffffff, Color.BLUE}, null, Shader.TileMode.CLAMP);
+                mPaint.setShader(mLinearGradient);
+                mMatrix = new Matrix();
+            }
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (mMatrix != null) {
+            mTranslate += mViewWidth / 5;
+            if (mTranslate > 2 * mViewWidth) {
+                mTranslate = -mViewWidth;
+            }
+            mMatrix.setTranslate(mTranslate,0);
+            mLinearGradient.setLocalMatrix(mMatrix);
+            postInvalidateDelayed(100);
+        }
+    }
+}
