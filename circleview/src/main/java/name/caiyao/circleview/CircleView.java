8d25733708391c776b2f879efa3a1a448e13c403
YiuChoi <whucaiyao@gmail.com>
2016-06-25 12:33:19 +0800

    圆形控件

--- circleview/src/main/java/name/caiyao/circleview/CircleView.java
+++ circleview/src/main/java/name/caiyao/circleview/CircleView.java
@@ -3,6 +3,8 @@ package name.caiyao.circleview;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.RectF;
 import android.os.Build;
 import android.util.AttributeSet;
@@ -17,6 +19,7 @@ public class CircleView extends View {
     private int mCircleXY;
     private float mRadius;
     private RectF mRectF;
+    private Paint circlePaint,mArcPaint,mTextPaint;
 
     public CircleView(Context context) {
         super(context);
@@ -43,11 +46,20 @@ public class CircleView extends View {
     private void init() {
         mRadius = (float) (getMeasuredWidth() * 0.5 / 2);
         mCircleXY = getMeasuredWidth() / 2;
+        mRectF = new RectF((float) (getMeasuredWidth() * 0.1), (float) (getMeasuredWidth() * 0.1), (float) (getMeasuredWidth() * 0.9), (float) (getMeasuredWidth() * 0.9));
+        circlePaint = new Paint();
+        circlePaint.setColor(Color.YELLOW);
+        mArcPaint = new Paint();
+        mArcPaint.setColor(Color.BLUE);
+        mTextPaint = new Paint();
+        mTextPaint.setColor(Color.GREEN);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
+        canvas.drawCircle(mCircleXY,mCircleXY,mRadius,circlePaint);
+        canvas.drawArc(mRectF,270,25,false,mArcPaint);
+        canvas.drawText("sdsd",0,4,mCircleXY,mCircleXY+(4/4),mTextPaint);
         super.onDraw(canvas);
-        mRectF = new RectF((float) (getMeasuredWidth() * 0.1), (float) (getMeasuredWidth() * 0.1), (float) (getMeasuredWidth() * 0.9), (float) (getMeasuredWidth() * 0.9));
     }
 }
